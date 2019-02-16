bc772309d2b7cd2f5732355e40a02af16f7b495e
root <dominik.obuch@wp.pl>
2019-02-16 18:24:58 +0100

    builded prev version

--- app/js/header.js
+++ app/js/header.js
@@ -7,3 +7,12 @@ window.addEventListener("scroll", () =>{
    
     }
 })
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
